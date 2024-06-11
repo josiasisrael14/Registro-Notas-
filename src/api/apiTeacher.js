ec91bc60991a5eb692c6f545cffaec1125a20516
Jairo Ayllon <jairo.josias14@gmail.com>
2024-06-11 16:00:54 -0500

    fix: consumir api crear profesor

--- src/api/apiTeacher.js
+++ src/api/apiTeacher.js
@@ -13,3 +13,17 @@ export const getWhereAllTeacher = () => {
       })
     })
   }
+
+  export const createDataTeacher = (newRow) => {
+    return new Promises((resolve, reject) => {
+      axios.post(`http://127.0.0.1:8080/teacher`, newRow)
+        .then(data => { 
+          console.log('data', data);
+          resolve(data.data);
+        })
+        .catch(error => {
+          console.log('error', error);
+          reject(error);
+        });
+    });
+  }
